77b9656140c5c5ad517b0370eba5e90e05c5b8e3
Xparda007 <nightwish_dream@hotmail.com>
2024-08-16 19:48:33 +0000

    tratando

--- holaraul/src/App.jsx
+++ holaraul/src/App.jsx
@@ -1,39 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ciberteclogo from  './image/ciberteclogo.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+import { Fragment } from 'react';
+import titulo from './componentes/titulo';
+
+
+import { Fragment } from 'react';
+import titulo from './componentes/titulo';
+
+
+
+function App(){
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.cibertec.edu.pe/" target="_blank">
-          <img src={ciberteclogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Raul + Vite + React + cibertec</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+    <>
+    <titulo/>
+    </>
+  );
+}
+export default App;
--- holaraul/src/componentes/titulo.jsx
+++ holaraul/src/componentes/titulo.jsx
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 function titulo(){
     return(
         <>
