2847d7f97ffc95d90df9d6a7dd53ed2507555631
Xparda007 <nightwish_dream@hotmail.com>
2024-08-16 18:56:10 +0000

    creado componente

---
+++ holaraul/src/componentes/titulo.jsx
@@ -0,0 +1,10 @@
+function titulo(){
+    return(
+        <>
+        <h1>Conoce el peru primero</h1>
+        <h2>En el Peru existen hermosos lugares para conocer y disfrutar</h2>
+        </>
+
+    );
+}
+export default titulo;
